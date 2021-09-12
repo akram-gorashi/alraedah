5b718854e93a7d52159e9fda47bf72bcf21d054e
Akram Gorashi <akramgorashi6@hotmail.com>
2021-09-12 02:18:03 +0200

    [update] added header and landing page and footer bootstrap and font awesome cdns

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page/landing-page.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeroComponent } from './components/landing-page/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingPageComponent,
+    HeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([]),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
