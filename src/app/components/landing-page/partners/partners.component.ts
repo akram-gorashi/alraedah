827333ff3c6fdbecd09b3e922c73579432e6225a
Akram Gorashi <akramgorashi6@hotmail.com>
2021-09-15 19:26:58 +0200

    [update] added partners section

--- src/app/components/landing-page/partners/partners.component.ts
+++ src/app/components/landing-page/partners/partners.component.ts
@@ -10,37 +10,40 @@ export class PartnersComponent implements OnInit {
 /* -------------------------------------------------------------------------- */
  /*                             parteners carousol                             */
  /* -------------------------------------------------------------------------- */
- partnersCarouselOpts: OwlOptions = {
-  margin: 0,
-  loop: true,
-  mouseDrag: true,
-  touchDrag: true,
-  autoplay: true,
-  autoplayTimeout: 3000,
-  autoplayHoverPause: true,
-  dots: false,
-  // nav: true,
-  navSpeed: 700,
-  // navText: ['&#8249', '&#8250;'],
-  responsive: {
-     0: {
-        items: 2,
-     },
-     400: {
-        items: 2,
-     },
-     500: {
-        items: 3,
-     },
-     700: {
-        items: 3,
-     },
-     900: {
-        items: 5,
-     },
-  },
-  responsiveRefreshRate: 50,
-};
+
+   /* Partners Carousel Opts */
+   partnersCarouselOpts: OwlOptions = {
+      margin: 0,
+      loop: true,
+      mouseDrag: true,
+      touchDrag: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      autoplayHoverPause: true,
+      dots: false,
+      // nav: true,
+      navSpeed: 700,
+      // navText: ['&#8249', '&#8250;'],
+      responsive: {
+         0: {
+            items: 2,
+         },
+         400: {
+            items: 2,
+         },
+         500: {
+            items: 3,
+         },
+         700: {
+            items: 3,
+         },
+         900: {
+            items: 5,
+         },
+      },
+      responsiveRefreshRate: 50,
+   };
+   
 
   constructor() { }
 
